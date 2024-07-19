46d05290b7947253bce1dafd7a7a3c5bc6a3a8be
starry <115192496+sky22333@users.noreply.github.com>
2024-07-19 23:04:17 +0800

    优化版本和mysql

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ return [
     'host' => '127.0.0.1',
     'database' => 'demo',
     'username' => 'demo',
-    'password' => 'TbfXmL2JTcXYYrWZ',
+    'password' => '123456',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => 'acg_',
