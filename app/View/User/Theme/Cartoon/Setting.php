0c5c338d265f4fa8e06cdcc60ee70c6857cc19da
starry <115192496+sky22333@users.noreply.github.com>
2024-10-16 08:58:32 +0800

    1

--- app/View/User/Theme/Cartoon/Setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare (strict_types=1);
-
-return [
-    'style' => '2',
-    'icp' => '',
-    'cache' => '0',
-    'cache_expire' => '60',
-];
--- config/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare (strict_types=1);
-
-return [
-    'version' => '1.3.8'
-];
