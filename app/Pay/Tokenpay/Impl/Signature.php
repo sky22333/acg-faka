9e7f10fdbe3290310b9585fc9229ab12611d44bb
starry <115192496+sky22333@users.noreply.github.com>
2024-11-01 02:00:16 +0800

    Update Signature.php

--- app/Pay/Tokenpay/Impl/Signature.php
+++ app/Pay/Tokenpay/Impl/Signature.php
@@ -1,52 +1,58 @@
 <?php
 declare(strict_types=1);
-
 namespace App\Pay\Tokenpay\Impl;
 
-/**
- * Class Signature
- * @package App\Pay\Kvmpay\Impl
- */
+use Kernel\Util\Context;
+
 class Signature implements \App\Pay\Signature
 {
+   /**
+    * 生成签名
+    * @param array $data
+    * @param string $key
+    * @return string
+    */
+   public static function generateSignature(array $parameter, string $signKey): string
+   {
+       unset($parameter['Signature']);
+       
+       ksort($parameter);
+       
+       $sign = '';
+       foreach ($parameter as $key => $val) {
+           if ($val === '') continue;
+           if ($sign !== '') {
+               $sign .= '&';
+           }
+           $sign .= "$key=$val";
+       }
+       
+       return md5($sign . $signKey);
+   }
 
-    /**
-     * 生成签名
-     * @param array $data
-     * @param string $key
-     * @return string
-     */
-    public static function generateSignature(array $parameter, string $signKey): string
-    {
-        ksort($parameter);
-        reset($parameter);
-        $sign = '';
-        $urls = '';
-        foreach ($parameter as $key => $val) {
-            if ($val == '') continue;
-            if ($key != 'Signature') {
-                if ($sign != '') {
-                    $sign .= "&";
-                    $urls .= "&";
-                }
-                $sign .= "$key=$val";
-                $urls .= "$key=" . urlencode((string)$val);
-            }
-        }
-        $sign = md5($sign . $signKey);
-        return $sign;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function verification(array $data, array $config): bool
-    {
-        $sign = $data['Signature'];
-        $generateSignature = self::generateSignature($data, $config['key']);
-        if ($sign != $generateSignature) {
-            return false;
-        }
-        return true;
-    }
+   /**
+    * @inheritDoc
+    */
+   public function verification(array $data, array $config): bool
+   {
+       // 获取原始POST数据
+       $rawData = json_decode(file_get_contents('php://input'), true);
+       if (!$rawData) {
+           $rawData = $data;
+       }
+       
+       if (!isset($rawData['Signature'])) {
+           return false;
+       }
+       
+       $sign = $rawData['Signature'];
+       $generateSignature = self::generateSignature($rawData, $config['key']);
+       
+       if ($sign !== $generateSignature) {
+           return false;
+       }
+       
+       Context::set(\App\Consts\Pay::DAFA, $rawData);
+       return true;
+   }
 }
